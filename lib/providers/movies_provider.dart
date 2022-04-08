d237f717633420512471d4e55281ff9cd9a667dd
Carlos Vazquez <carlos.vazquez.unach@gmail.com>
2022-04-07 20:15:03 -0500

    Mapeo de data

--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -1,12 +1,17 @@
+import 'package:http/http.dart';
+import 'package:peliculas/models/now_playing_responde.dart';
 import 'package:peliculas/providers/provider_base.dart';
 
 class MoviesProvider extends ProviderBase {
   MoviesProvider() : super('3/movie/now_playing') {
     obtenerPeliculas();
   }
+
   ///Retorna toda la información sobre las peliculas en cartelera
   obtenerPeliculas() async {
-    final respuesta =
+    final Response respuesta =
         await client.get(urlBase, params: {'language': 'es-ES', 'page': '1'});
+    final NowPlayingResponse peliculasResponse =
+        NowPlayingResponse.fromJson(respuesta.body);
   }
 }
